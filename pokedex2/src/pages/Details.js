241c010ff1ad517364db5f94a4a7d56e43b003f5
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-06-14 16:19:44 -0300

    header e coordinator

--- pokedex2/src/pages/Details.js
+++ pokedex2/src/pages/Details.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Header } from '../components/header/header'
+import { goToHome } from '../router/Coordinator';
+import { useHistory } from "react-router-dom";
 
 const Details = () => {
+    const history = useHistory();
     return (
         <div>
-            Details
+           <Header>
+                <button onClick={() => goToHome(history)}>Voltar</button> 
+                <h3>Nome do pokemon</h3>
+                <button>Adicionar/Remover da pokedex</button> 
+            </Header>
+            Details 
         </div>
     )
 }
