5723d9e86c8b7c35772eb0ed81a7534490717711
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-14 16:35:29 -0300

    pagina detalhe em construção

--- pokedex2/src/pages/Details.js
+++ pokedex2/src/pages/Details.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const FakeCard = styled.div`
+    background-color: red;
+    border: 1px black solid;
+    height:100%;
+`
+
+const Div = styled.div`
+    display:flex;
+    height:100%;
+    width:100%;
+    padding:2rem;
+`
+
+const ImageArea = styled.div`
+    height:70%;
+`
+
 
 const Details = () => {
     return (
-        <div>
-            Details
-        </div>
+        <Div>
+            <ImageArea>
+                <FakeCard>
+                    <h1> Imagem Frontal </h1>
+                </FakeCard>
+                <FakeCard>
+                    <h1> Imagem de Costas </h1>
+                </FakeCard>
+            </ImageArea>
+            <FakeCard id = "stats">
+                <h1>Stats</h1>
+            </FakeCard>
+            <div>
+                <FakeCard>
+                    <h1>Tipos</h1>
+                </FakeCard>
+                <FakeCard>
+                    <h1>Moves</h1>
+                </FakeCard>
+            </div>
+        </Div>
+        
     )
 }
 
