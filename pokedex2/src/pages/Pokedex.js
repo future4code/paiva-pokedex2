541665679c637dbf82d3a997bdd1bf9ff6e56492
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-15 17:15:16 -0300

    custom Hook em construção

--- pokedex2/src/pages/Pokedex.js
+++ pokedex2/src/pages/Pokedex.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import useDetails from '../hooks/useDetails'
+import usePokemon from '../hooks/usePokemon'
 
 const Pokedex = () => {
+    const [details,getPokeDetail] = useDetails()
+
+    const pokemon = usePokemon()
+
+    console.log(pokemon)
+
+    // const listPokemon = pokemon.map((poke) => {
+    //     return(
+    //         <div>
+    //             <h2>{poke}</h2>
+    //         </div>
+    //     )
+    // })
+    
     return (
         <div>
-            Pokedex
+            {/* {listPokemon} */}
         </div>
     )
 }
