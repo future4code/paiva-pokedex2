f214b7276cd4d9a71e0c4b0417cfe8e2489ec27c
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-17 15:43:29 -0300

    Card dinamico

--- pokedex2/src/components/PokeCard/PokeCard.js
+++ pokedex2/src/components/PokeCard/PokeCard.js
@@ -1,16 +1,24 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import { CardContainer, ImgContainer, PokeImg, ButtonContainer } from './styled'
 import { GlobalStateContext } from '../../global/GlobalStateContext'
 import { goTo } from '../../router/Coordinator'
 import { useHistory } from 'react-router-dom'
 
-
-
-
 const PokeCard = (props) => {
     const {pokedex, setPokedex} = useContext(GlobalStateContext)
-
     const history = useHistory()
+
+    useEffect(() => {
+        checkPokedex()
+    } , [pokedex])
+
+    const checkPokedex = () => {
+        if(pokedex.find(poke => poke.name === props.name)){
+            return true
+        }else{
+            return false
+        }
+    }
       
     const addPokemon = (name, image) => {
         const poke = {name: name, image: image }
@@ -31,7 +39,7 @@ const PokeCard = (props) => {
              />
             </ImgContainer>
             <ButtonContainer>
-                <button onClick={()=> props.pokedex ? (removePokemom(props.name)) : (addPokemon(props.name, props.pokeImg))} >Adicionar a Pokedex</button>
+                <button onClick={()=> checkPokedex() ? (removePokemom(props.name)) : (addPokemon(props.name, props.pokeImg))} >{checkPokedex() ? "Remover da Pokedex":"Adicionar a Pokedex"}</button>
                 <button onClick = {() => goTo(history,`/details/${props.name}`)}>Detalhes</button>
             </ButtonContainer>
         </CardContainer>
