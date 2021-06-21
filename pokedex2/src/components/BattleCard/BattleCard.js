37ba8817416c3ce6dbd4119c080914b336ded9d3
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-21 00:10:40 -0300

    Alert para versão mobile corrigido

--- pokedex2/src/components/BattleCard/BattleCard.js
+++ pokedex2/src/components/BattleCard/BattleCard.js
@@ -6,8 +6,11 @@ import { CardBattle, ImageBattle } from "./style"
 function BattleCard(props) {
     const {pokeDetails,Capitalize} = useContext(GlobalStateBattle)
     const [mobileStatus,setMobileStatus] = useState(false)
+    
+    useEffect(() => {
+        alert("Caso esteja usando modo mobile do navegador, atualize a pagina para carregar versão Mobile")
+    },[])
 
-    alert("Caso esteja usando modo mobile do navegador, atualize a pagina para carregar versão Mobile")
 
     useEffect(() => [
         setMobileStatus(false)
